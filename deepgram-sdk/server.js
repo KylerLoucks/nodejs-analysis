275c05c540a37568beb0408f42a01ea2021d7c0d
cloud303-kloucks <kloucks@cloud303.io>
2023-05-19 21:46:29 -0700

    Added aws translate

--- deepgram-sdk/server.js
+++ deepgram-sdk/server.js
@@ -3,9 +3,11 @@
 const { Server } = require("socket.io");
 const { createAdapter } = require("@socket.io/redis-adapter");
 const redis = require("redis");
-
-
 require('dotenv').config();
+
+const AWS = require('aws-sdk')
+AWS.config.update({ region: 'us-east-1' });
+const translate = new AWS.Translate();
 
 // Deepgram SDK
 const { Deepgram } = require('@deepgram/sdk')
@@ -26,56 +28,68 @@ io.listen(3001);
 console.log("websocket listening on port 3001")
 
 
-// establish connection to redis
-pubClient.connect()
+// // establish connection to redis
+// pubClient.connect()
 
-// handle redis connection errors
-pubClient.on('error', (err) => {
-    console.log('Error occured while connecting or accessing redis server');
-});
+// // handle redis connection errors
+// pubClient.on('error', (err) => {
+//     // console.log('Error occured while connecting or accessing redis server');
+// });
 
 
+const deepgram = new Deepgram(process.env.DG_KEY)
 // Connection is established between client and server:
 io.on("connection", async (socket) => {
     console.log(`A user connected to the Websocket!! ${socket.id}`)
 
 
     // TODO: put in io.on("join-meeting")
-    const response = await pubClient.get(`meetingId`).catch((err) => {
-        console.log("Error getting data in redis: ", err)
-    });
+    // const response = await pubClient.get(`meetingId`).catch((err) => {
+    //     console.log("Error getting data in redis: ", err)
+    // });
 
-    const data = JSON.parse(response || "{}");
-    data.users = data.users || [];
-    data.users.push({ cognitoUser: 'test', languagePref: "en" })
+    // const data = JSON.parse(response || "{}");
+    // data.users = data.users || [];
+    // data.users.push({ cognitoUser: 'test', languagePref: "en" })
 
 
-    const updatedData = JSON.stringify(data)
-    console.log(updatedData)
+    // const updatedData = JSON.stringify(data)
+    // console.log(updatedData)
 
-    // update redis data
-    await pubClient.set(`meetingId`, JSON.stringify(updatedData)).catch(err =>
-        console.log("Error setting data in redis: ", err)
-    );
+    // // update redis data
+    // await pubClient.set(`meetingId`, JSON.stringify(updatedData)).catch(err =>
+    //     console.log("Error setting data in redis: ", err)
+    // );
 
-    const deepgram = new Deepgram(process.env.DG_KEY)
     const deepgramLive = deepgram.transcription.live({ 
         punctuate: true,
         utterances: true,
-        model: "nova"
+        // model: "nova"
         
     })
 
     // listen for deepgram transcripts callback:
     deepgramLive.addListener('transcriptReceived', (data) => {
-        console.log("transcript data: ", data)
+        console.log("Recieved Transcript from Deepgram")
         socket.emit("transcript", data)
         
+        // const received = JSON.parse(data)
+        // const transcript = received?.channel?.alternatives[0]?.transcript ?? null;
+        // // if the transcription text recieved is fully processed
+        // if (transcript && received.is_final) {
+        //     translateText(transcript, "en", "es")
+        //         .then(translatedText => {
+        //             console.log('Translated text:', translatedText);
+        //         })
+        //         .catch(error => {
+        //             console.error('Translation error:', error);
+        //         });
+        // }
     })
     
     // handle when deepgram connection is established
     deepgramLive.addListener("open", () => {
-        console.log('dg onopen');
+        console.log('deepgram connected!');
         
     });
 
@@ -86,17 +100,13 @@ io.on("connection", async (socket) => {
 
     // Listen for audio stream
     socket.on("audiostream", (event) => {
-        // console.log("event recieved", event)
+        console.log("event recieved")
         // check if connection is established with deepgram
+        console.log(deepgramLive.getReadyState())
         if (deepgramLive.getReadyState() == 1) {
             deepgramLive.send(event.data)
         }
     });
-
-    // translate text to another language
-    socket.on("translate", (event) => {
-        translate()
-    })
 
     // handle when users disconnect
     socket.on("disconnect", (reason) => {
@@ -107,7 +117,35 @@ io.on("connection", async (socket) => {
 
 });
 
-async function translate(source, target, text) {
+
+
+/**
+ * Translates text-to-text
+ * @param {String} text 
+ * @param {String} sourceLanguageCode 
+ * @param {String} targetLanguageCode 
+ * @returns {String} translated text
+ */
+async function translateText(text, sourceLanguageCode, targetLanguageCode) {
+    const params = {
+      Text: text,
+      SourceLanguageCode: sourceLanguageCode,
+      TargetLanguageCode: targetLanguageCode
+    };
+  
+    try {
+      const result = await translate.translateText(params).promise();
+      return result.TranslatedText;
+    } catch (error) {
+      console.error('Translation error:', error);
+      throw error;
+    }
+}
+
+
+
+
+async function itranslate(source, target, text) {
     const url = 'https://dev-api.itranslate.com/translation/v2/'
     const headers = {
       Authorization: 'YOUR ITRANSLATE API KEY',
@@ -125,5 +163,5 @@ async function translate(source, target, text) {
     }).then((r) => r.json())
   
     return result
-  }
+}
 
